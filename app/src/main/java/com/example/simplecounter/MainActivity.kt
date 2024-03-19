bf006d17aee210a0801aa69b13445b59faca6413
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-03-19 12:07:32 +0800

    first commit

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.simplecounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simplecounter.ui.theme.SimpleCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleCounterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Greeting("Android")
+                    Counter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimpleCounterTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Counter(){
+    var number = remember { mutableStateOf(1000) }
+
+
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = number.value.toString(),
+            fontSize = 24.sp
+        )
+
+        Button(onClick = {
+            println("clicked")
+            number.value--
+        }) {
+            Text("Click me")
+        }
+
+    }
+}
+
+
+
+
